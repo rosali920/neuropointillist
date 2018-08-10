4149a20fe71b13394475b037267fa86472b7fb0c
Rosa Li <rosali920@gmail.com>
2018-08-10 16:24:10 -0700

    tutorial folder using open data

--- example.testretest/fmrimodel.R
+++ example.testretest/fmrimodel.R
@@ -5,21 +5,20 @@ library(nlme)
 
 processVoxel <-function(v) {
     Y <- voxeldat[,v]
-    e <- try(mod <- lme(Y ~ task, random=~1|sub, method=c("ML"), na.action=na.omit, corr=corAR1(form=~1|sub), control=lmeControl(returnObject=TRUE,singular.ok=TRUE)))
+    e <- try(mod <- lme(Y ~ task, random=~1|sub, method=c("ML"), na.action=na.omit))
     if(inherits(e, "try-error")) {
         mod <- NULL
     }
     if(!is.null(mod)) {
-        contr <- c(0, 1)
-        out <- anova(mod,L=contr)
-        t.stat <- (t(contr)%*%mod$coefficients$fixed)/sqrt(t(contr)%*%vcov(mod)%*%contr)
-        p <- 1-pt(t.stat,df=out$denDF)
-        retvals <- list(summary(mod)$tTable["task", "t-value"])
+    mod.tt <- summary(mod)$tTable
+    retvals <- list(
+    mod.tt["task", "p-value"],
+          mod.tt["task", "t-value"])
     } else {
-    # If we are returning 4 dimensional data, we need to be specify how long
-    # the array will be in case of errors
-        retvals <- list(999,999,999,999)
+    # If we are returning 2 dimensional data, we need to be specify how long
+    # the array will be in case of errors.
+        retvals <- list(999, 999)
     }
-    names(retvals) <- c("tstat-Task")
+    names(retvals) <- c("p-task", "t-task")
     retvals
 }
--- example.testretest/readargs.R
+++ example.testretest/readargs.R
@@ -3,5 +3,6 @@ cmdargs <- c("-m","group_min_mask.nii.gz", "--set1", "setfilenames1.txt",
              "--setlabels1", "setlabels1.csv",
              "--setlabels2", "setlabels2.csv",
              "--model", "fmrimodel.R",
-             "--output", "testretest/model1",
-             "--debug", "debug.Rdata")
+             "--output", "testretestREM/model1_",
+             "--debug", "debug.Rdata",
+             "--sgeN", "1")
